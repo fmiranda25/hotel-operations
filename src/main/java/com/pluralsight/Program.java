1722c54eafcaeff8e6d31bbef02934f8edeb8ac8
Fernando <f.miranda25d@gmail.com>
2026-05-06 13:48:58 -0500

    First iteration of exercise classes work

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,12 +1,23 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Program {
-    static Room room1 = new Room();
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        room1.setSize("king");
+
+        Room room1 = new Room("king", 139, false, false, true);
         System.out.println(room1.getSize());
         System.out.println(room1.getPrice());
+        System.out.println(room1.isOccupied());
+        System.out.println(room1.isDirty());
+        System.out.println(room1.isAvailable());
 
+        Reservation reservation1 = new Reservation("king", 139, 2, true);
+        System.out.println(reservation1.getPrice());
+
+        Employee employee1 = new Employee("Turnip", "Fernando", "Miranda", "QA", 20.00, 41);
+        System.out.println(employee1.getTotalPay());
+        System.out.println(employee1.getFullName());
 
     }
 }
